9c6b122b18142ff206cca3f455495b6f2f08bfa2
qiuxin2012 <qiuxin2012cs@gmail.com>
2019-10-15 09:50:27 +0800

    zoo

--- mnist/main.py
+++ mnist/main.py
@@ -5,6 +5,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from zoo.pipeline.api.net.torch_net import TorchNet
+from zoo.pipeline.api.net.torch_criterion import TorchCriterion
+from zoo.pipeline.estimator import *
+from bigdl.optim.optimizer import SGD, Adam
+from zoo.common.nncontext import *
+from zoo.feature.common import FeatureSet
+from bigdl.optim.optimizer import Loss
+from zoo.pipeline.api.keras.metrics import Accuracy
 
 
 class Net(nn.Module):
@@ -25,37 +33,34 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
     
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
+def train(args, estimator, zooCriterion, train_loader, epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
+        estimator.train_minibatch(data.numpy(), target.numpy().astype(np.int32), zooCriterion)
+        # data, target = data.to(device), target.to(device)
+        # optimizer.zero_grad()
+        # output = model(data)
+        # loss = F.nll_loss(output, target)
+        # loss.backward()
+        # optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\t'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+                100. * batch_idx / len(train_loader)))
 
-def test(args, model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
+def test(args, estimator, zooCriterion, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            f = FeatureSet.minibatch([[data.numpy()]], [[target.numpy()]])
+            # output = model(data)
+            # test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            # pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            # correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    # test_loss /= len(test_loader.dataset)
+    #
+    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #     test_loss, correct, len(test_loader.dataset),
+    #     100. * correct / len(test_loader.dataset)))
 
 def main():
     # Training settings
@@ -105,9 +110,28 @@ def main():
     model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
+    sc = init_nncontext(init_spark_conf().setMaster("local[8]").set("spark.driver.memory", "10g"))
+    model.train()
+    sgd = Adam()
+    zooModel = TorchNet.from_pytorch(model, [64, 1, 28, 28])
+    def lossFunc(input, target):
+        return nn.NLLLoss().forward(input, target.flatten().long())
+
+    zooCriterion = TorchCriterion.from_pytorch(lossFunc, [1, 2], torch.LongTensor([1]))
+    estimator = Estimator(zooModel, optim_methods=sgd)
+
+    v_input = []
+    v_target = []
+    for data, target in test_loader:
+        v_input.append([data.numpy()])
+        v_target.append([target.numpy()])
+
+    test_featureset = FeatureSet.minibatch(v_input, v_target)
+
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        train(args, estimator, zooCriterion, train_loader, epoch)
+        # test(args, estimator, zooCriterion, test_featureset)
+        estimator.evaluate_minibatch(test_featureset, [Loss(zooCriterion), Accuracy()])
 
     if (args.save_model):
         torch.save(model.state_dict(),"mnist_cnn.pt")
